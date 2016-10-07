ed6e3c1b75f083e14c9df8bfce910ea79deb5178
pahanbl4 <pashagozasibett@gmail.com>
2016-10-07 16:29:02 +0300

    vk

---
+++ houmi22/app/src/main/java/com/example/pasha_000/houmi2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pasha_000.houmi2;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    static int a=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+    public void start_Main2(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    }
